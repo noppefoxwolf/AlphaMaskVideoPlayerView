109dd595bfe3dc51b5fa20216692182b310fcef8
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-11-16 14:25:53 +0900

    カメラプレビューをサンプルに追加

--- Example/AlphaMaskVideoPlayerView/ViewController.swift
+++ Example/AlphaMaskVideoPlayerView/ViewController.swift
@@ -8,14 +8,26 @@
 
 import UIKit
 import AlphaMaskVideoPlayerView
+import AVFoundation
 
 final class ViewController: UIViewController, AlphaMaskVideoPlayerDelegate {
   let player = AlphaMaskVideoPlayer(mainVideoUrl: Bundle.main.url(forResource: "main", withExtension: "mp4")!,
                                     alphaVideoUrl: Bundle.main.url(forResource: "main_alpha", withExtension: "mp4")!)
   @IBOutlet private weak var playerView: AlphaMaskVideoPlayerView!
   
+  private let camera = AVCaptureDevice.default(for: AVMediaType.video)
+  private lazy var input: AVCaptureDeviceInput = try! .init(device: camera!)
+  private let session = AVCaptureSession()
+  private lazy var preview = AVCaptureVideoPreviewLayer(session: self.session)
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    preview.frame = view.bounds
+    view.layer.insertSublayer(preview, at: 0)
+    session.addInput(input)
+    session.startRunning()
+    
     playerView.setPlayer(player)
     playerView.contentMode = .scaleAspectFill
     player.delegate = self
