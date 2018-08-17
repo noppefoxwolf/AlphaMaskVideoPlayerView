93f2e6bc1662c1bba1a5f94e96aaec509462b322
tomoya.hirano <tomoya.hirano@dena.com>
2018-08-17 19:57:15 +0900

    ignore cahce

--- AlphaMaskVideoPlayerView/Classes/AlphaMaskVideoPlayerView.swift
+++ AlphaMaskVideoPlayerView/Classes/AlphaMaskVideoPlayerView.swift
@@ -7,56 +7,55 @@
 
 import GLKit
 
-open class AlphaMaskVideoPlayerView: UIImageView, AlphaMaskVideoPlayerUpdateDelegate {
-  private lazy var ciContext: CIContext = .init(eaglContext: EAGLContext(api: .openGLES2)!, options: [kCIContextWorkingColorSpace : NSNull(), kCIContextCacheIntermediates : true])
-//  private var image: CIImage? = nil
+open class AlphaMaskVideoPlayerView: GLKView, AlphaMaskVideoPlayerUpdateDelegate {.init
+  private lazy var ciContext: CIContext = .init(eaglContext: context, options: [kCIContextWorkingColorSpace : NSNull(), kCIContextCacheIntermediates : false])
+  private var image: CIImage? = nil
   private var threadSafeBounds: CGRect = CGRect.zero
   private var threadSafeContentMode: UIViewContentMode = UIViewContentMode.scaleToFill
   private var player: AlphaMaskVideoPlayer? = nil
   
   open override var bounds: CGRect {
-    
     didSet { threadSafeBounds = bounds }
   }
   open override var contentMode: UIViewContentMode {
     didSet { threadSafeContentMode = contentMode }
   }
   
-//  public override convenience init(frame: CGRect) {
-//    let eaglContext = EAGLContext(api: .openGLES2)!
-//    eaglContext.isMultiThreaded = true
-//    self.init(frame: frame, context: eaglContext)
-//
-//  }
+  public override convenience init(frame: CGRect) {
+    let eaglContext = EAGLContext(api: .openGLES2)!
+    eaglContext.isMultiThreaded = true
+    self.init(frame: frame, context: eaglContext)
+    
+  }
   
-//  public override init(frame: CGRect, context: EAGLContext) {
-//    super.init(frame: frame, context: context)
-//    enableSetNeedsDisplay = false
-//    backgroundColor = .clear
-//    _ = destRect
-//  }
-//
-//  required public init?(coder aDecoder: NSCoder) {
-//    super.init(coder: aDecoder)
-//    context = EAGLContext(api: .openGLES2)!
-//    context.isMultiThreaded = true
-//    enableSetNeedsDisplay = false
-//    backgroundColor = .clear
-//    _ = destRect
-//    contentScaleFactor = 1.0
-//  }
-//
-//  open override func draw(_ rect: CGRect) {
-//    guard UIApplication.shared.applicationState != .background else { return }
-//    glClearColor(0, 0, 0, 0)
-//    if let image = image {
-//      ciContext.draw(image,
-//                     in: destRect(from: image.extent).applying(CGAffineTransform(scaleX: contentScaleFactor, y: contentScaleFactor)),
-//                     from: image.extent)
-//    } else {
-//      glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
-//    }
-//  }
+  public override init(frame: CGRect, context: EAGLContext) {
+    super.init(frame: frame, context: context)
+    enableSetNeedsDisplay = false
+    backgroundColor = .clear
+    _ = destRect
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    context = EAGLContext(api: .openGLES2)!
+    context.isMultiThreaded = true
+    enableSetNeedsDisplay = false
+    backgroundColor = .clear
+    _ = destRect
+    contentScaleFactor = 1.0
+  }
+  
+  open override func draw(_ rect: CGRect) {
+    guard UIApplication.shared.applicationState != .background else { return }
+    glClearColor(0, 0, 0, 0)
+    if let image = image {
+      ciContext.draw(image,
+                     in: destRect(from: image.extent).applying(CGAffineTransform(scaleX: contentScaleFactor, y: contentScaleFactor)),
+                     from: image.extent)
+    } else {
+      glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+    }
+  }
   
   public func setPlayer(_ player: AlphaMaskVideoPlayer) {
     self.player = player
@@ -64,21 +63,19 @@ open class AlphaMaskVideoPlayerView: UIImageView, AlphaMaskVideoPlayerUpdateDele
   }
   
   public func didOutputFrame(_ image: CIImage?) {
-    guard let image = image else { return }
-    self.image = UIImage(ciImage: image)
-//    EAGLContext.setCurrent(context)
-//    self.image = image
-//    DispatchQueue.global(qos: .userInteractive).sync { [weak self] in
-//      self?.display()
-//    }
+    EAGLContext.setCurrent(context)
+    self.image = image
+    DispatchQueue.global(qos: .userInteractive).sync { [weak self] in
+      self?.display()
+    }
   }
   
   func didReceiveError(_ error: Error?) {
     self.image = nil
-//    print("[AlphaMaskVideoPlayerView didReceiveError] ", error ?? "unknown error")
-//    DispatchQueue.global(qos: .userInteractive).sync { [weak self] in
-//      self?.display()
-//    }
+    print("[AlphaMaskVideoPlayerView didReceiveError] ", error ?? "unknown error")
+    DispatchQueue.global(qos: .userInteractive).sync { [weak self] in
+      self?.display()
+    }
     
   }
   open override func layoutSubviews() {
